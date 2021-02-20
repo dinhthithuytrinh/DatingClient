480dd28681164159d517f9e9d0730688a76c5175
lekyvong09 <lekyvong09@gmail>
2021-02-20 19:37:01 +0700

    complete user registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -10,10 +11,12 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  // model: any = {};
   registerForm: FormGroup;
+  validationErrors: string[] = [];
 
-  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder) { }
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder,
+          private router: Router) { }
 
   ngOnInit(): void {
     this.intitializeForm();
@@ -43,7 +46,11 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    console.log(this.registerForm.value);
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl('/members');
+    }, error => { 
+      this.validationErrors = error;
+    });
     // this.accountService.register(this.model).subscribe(response => {
     //   console.log(response);
     //   this.cancel();
