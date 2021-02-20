2a9bb577b3cd3af28f6bf6392fdb9dc133f89271
lekyvong09 <lekyvong09@gmail>
2021-02-20 18:41:20 +0700

    add date input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from "ngx-spinner";
 import { FileUploadModule } from 'ng2-file-upload';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -69,6 +70,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     }),
     TabsModule.forRoot(),
     NgxGalleryModule,
+    BsDatepickerModule.forRoot(),
     NgxSpinnerModule
   ],
   providers: [
