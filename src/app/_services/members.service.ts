b005927d9091cd4578f1a4e0d4b5e35f9b63e513
lekyvong09 <lekyvong09@gmail>
2021-02-27 16:16:53 +0700

    get a specific member from the caches

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -62,8 +62,12 @@ export class MembersService {
   }
 
   getMember(username: string): Observable<Member> {
-    const  member = this.members.find(x => x.username === username);
-    if (member !== undefined) return of(member);
+    const member = [...this.memberCache.values()].reduce((arr, elem) => arr.concat(elem.result),[]).find((member: Member) => member.username === username);
+    console.log(this.memberCache.values());
+    if (member) {
+      return of (member);
+    }
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
