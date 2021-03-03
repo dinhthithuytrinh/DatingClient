c134292b03164453a3bff84594f2e3f29ec2194c
lekyvong09 <lekyvong09@gmail>
2021-03-03 11:11:42 +0700

    set up Message component

--- src/app/_services/message.service.ts
+++ src/app/_services/message.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
+import { Message } from '../_models/message';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +14,8 @@ export class MessageService {
   constructor(private http: HttpClient) { }
 
   getMessage(pageNumber, pageSize, container) {
-    
+    let params = getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('Container', container);
+    return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http);
   }
 }
