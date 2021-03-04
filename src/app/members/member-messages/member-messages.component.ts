90d15f645804c070767118f554ad7e0ba8fd673c
lekyvong09 <lekyvong09@gmail>
2021-03-04 19:00:17 +0700

    sending message

--- src/app/members/member-messages/member-messages.component.ts
+++ src/app/members/member-messages/member-messages.component.ts
@@ -1,6 +1,8 @@
 import { Message } from 'src/app/_models/message';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MessageService } from 'src/app/_services/message.service';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-member-messages',
@@ -8,13 +10,21 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-messages.component.css']
 })
 export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm: NgForm;
   @Input() username: string;
   @Input() messages: Message[];
+  messageContent: string;
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
   }
 
+  sendMessage() {
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe(m => {
+      this.messages.push(m);
+      this.messageForm.reset();
+    })
+  }
 
 }
