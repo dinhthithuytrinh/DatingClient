d15d2ae12711020a4c2fe82280ef7185d5c0115d
lekyvong09 <lekyvong09@gmail>
2021-03-09 10:08:23 +0700

    Admin - hide Admin tab with directive using includes, some

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,6 +44,7 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/has-role.directive';
 
 @NgModule({
   declarations: [
@@ -62,7 +63,8 @@ import { AdminPanelComponent } from './admin-panel/admin-panel.component';
     MemberEditComponent,
     PhotoEditorComponent,
     MemberMessagesComponent,
-    AdminPanelComponent
+    AdminPanelComponent,
+    HasRoleDirective
   ],
   imports: [
     CommonModule,
