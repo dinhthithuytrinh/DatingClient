eed0aa2542cab5295923e97b04abde8a91ac200d
lekyvong09 <lekyvong09@gmail>
2021-03-14 17:02:56 +0700

    SignalR - Turn off reuse route to fix when navigate between message section of different users

--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -30,8 +30,10 @@ export class MemberDetailComponent implements OnInit, OnDestroy {
   user: User;
 
   constructor(public presence: PresenceService, private route: ActivatedRoute, 
-    private messageService: MessageService, private accountService: AccountService) { 
+    private messageService: MessageService, private accountService: AccountService,
+    private router: Router) { 
       this.accountService.currentUser$.pipe(take(1)).subscribe(response => this.user = response);
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
     }
 
   ngOnInit(): void {
